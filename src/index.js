e72215b55aacf93cf9cd6a00078f2e58303872c2
uchhit <101151228+karmacharyauchhit@users.noreply.github.com>
2023-08-18 14:44:00 +1000

    added required pages and redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <App />
+    <Toaster/>
+    </Provider>
   </React.StrictMode>
 );
 
