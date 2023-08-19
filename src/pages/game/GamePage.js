389fff17d5f8bdecfce4887188df7b9dd6ba725b
uchhit <101151228+karmacharyauchhit@users.noreply.github.com>
2023-08-19 13:31:11 +1000

    added and created required pages for css and js to properly display the styles and the coding methods connecting via exporting all the pages and components as required

--- src/pages/game/GamePage.js
+++ src/pages/game/GamePage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import Layout from "../../components/layout/Layout"
+import Board from "../../components/board/Board"
+import { Container } from "react-bootstrap"
 
 const GamePage = () => {
+  const query = new URLSearchParams(window.location.search)
+  const boardSize = parseInt(query.get("size")) || 15
+
   return (
-    <div>GamePage</div>
+    <Layout>
+      <Container className="py-5">
+        <Board boardSize={boardSize} />
+      </Container>
+    </Layout>
   )
 }
-
 export default GamePage
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import { useState } from "react"
+import Layout from "../../components/layout/Layout"
+import "./home.css"
+import { Container } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-hot-toast"
+import { useSelector } from "react-redux"
 const HomePage = () => {
+  const navigate = useNavigate()
+  const [boardSize, setBoardSize] = useState(null)
+  const sizes = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+  const { isAuthenticated } = useSelector((state) => state.user)
+
+  const handleStart = (boardSize) => {
+    if (!boardSize) {
+      toast.error("Please select a board size!")
+      return
+    }
+    isAuthenticated && boardSize
+      ? navigate(`/game?size=${boardSize}`)
+      : navigate(`/login?size=${boardSize}`)
+  }
+
   return (
-    <div>HomePage</div>
+    <Layout>
+      <Container className="home-container">
+        <div className="size-select-container">
+          <h1>Let&apos; Play</h1>
+          <i>Select a board size and press start</i>
+          <select
+            name=""
+            className="size-select"
+            onChange={(e) => setBoardSize(e.target.value)}
+          >
+            <option value="">Select Board Size</option>
+            {sizes?.map((size) => (
+              <option value={size} key={size}>
+                {size}
+              </option>
+            ))}
+          </select>
+          <button
+            type="button"
+            onClick={() => handleStart(boardSize)}
+            className="global-btn"
+          >
+            Start
+          </button>
+        </div>
+      </Container>
+    </Layout>
   )
 }
-
 export default HomePage
