1548c9f7362463d46e084a1519e38904b8abe35b
uchhit <101151228+karmacharyauchhit@users.noreply.github.com>
2023-09-24 00:37:46 +1000

    update on existing files for integration

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,5 @@
 import "./footer.css"
 const Footer = () => {
-  return (
-    <div className="footer">
-      &copy; All Rights Reserved by UNE| Developed by Uchhit Karmacharya
-    </div>
-  )
+  return <div className="footer">&copy; All Rights Reserved by UNE, Arthor Uchhit Karmacharya</div>
 }
 export default Footer
